773b5563adf5f893160c14f7d4f9a4f1d230fece
Teoneag <teodor.neagoe.climber@gmail.com>
2023-11-11 19:48:00 +0100

    tidy-step-1

---
+++ pb_5/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pb_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int count = 0;
+            int divisor = 2;
+
+            while (count < 2 && divisor <= n)
+            {
+                if (n % divisor == 0)
+                {
+                    sum += divisor;
+                    count++;
+                }
+
+                divisor++;
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
